f08e1388fd8f8529f6a35907a343eb8cdd158a76
Itsmahnor <mahnoorsarfraz23@gmail.com>
2025-08-29 18:37:43 +0500

    change

--- Frontend/Frontend/src/RegisterPage/SignUp/SignUp.jsx
+++ Frontend/Frontend/src/RegisterPage/SignUp/SignUp.jsx
@@ -236,7 +236,7 @@ const SignUp = () => {
                 name="programmingBackground"
                 value={formData.programmingBackground}
                 onChange={handleChange}
-                className="flex-grow py-2 bg-transparent text-[0.7rem] sm:text-[1rem] placeholder-[#b8b8b8] focus:outline-none"
+                className="flex-grow py-3 bg-transparent text-[0.7rem] sm:text-[1rem] placeholder-[#b8b8b8] focus:outline-none"
               >
                 <option value="" disabled className=''>
                   Do you have Programming Background?
